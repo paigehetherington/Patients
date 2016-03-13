ec54d77a0a3e17eb0d50fb4a5811c2d63f7e95b4
Paige Hetherington <herbmama@gmail.com>
2016-03-13 18:28:12 -0400

    updated "/" route

--- src/main/java/com/theironyard/services/PatientRepository.java
+++ src/main/java/com/theironyard/services/PatientRepository.java
@@ -1,12 +1,15 @@
 package com.theironyard.services;
 
+
 import com.theironyard.enitites.Patient;
 import com.theironyard.enitites.Practitioner;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by vajrayogini on 3/10/16.
  */
 public interface PatientRepository extends CrudRepository<Patient, Integer> {
-Patient findByPractitioner(Practitioner practitioner);
+List<Patient> findByPractitioner(Practitioner practitioner);
 }
