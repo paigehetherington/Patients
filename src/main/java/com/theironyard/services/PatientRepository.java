31f58db9cbf7fcc0c087f43d8b6be84e2ce0a16f
Paige Hetherington <herbmama@gmail.com>
2016-03-15 08:28:01 -0400

    updated patient repository for paging

--- src/main/java/com/theironyard/services/PatientRepository.java
+++ src/main/java/com/theironyard/services/PatientRepository.java
@@ -6,13 +6,14 @@ import com.theironyard.enitites.Practitioner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
 /**
  * Created by vajrayogini on 3/10/16.
  */
-public interface PatientRepository extends CrudRepository<Patient, Integer> {
+public interface PatientRepository extends PagingAndSortingRepository<Patient, Integer> {
     Page<Patient> findByPractitioner(Pageable pageable, Practitioner practitioner);
     Page<Patient> findAll(Pageable pageable);
     //List<Patient> findAllSortByNameStartingWithDesc(String name);
